1e46a4cea6570fdb9adf6d07a3c9e152209cd66b
Jesse <j.t.a.smith26@gmail.com>
2023-06-04 13:15:48 +1200

    Put glock onto shop1 page

---
+++ client/components/Shop.tsx
@@ -0,0 +1,18 @@
+//importing data from file path Data/data to use on shop page.
+
+import data from '../Data/data'
+
+function shop() {
+  return (
+    <>
+      <div>
+        <p>Shop1</p>
+      </div>
+      <div>
+        <img src="./public/images/Glock1.png" alt="This is a Glock"></img>
+      </div>
+    </>
+  )
+}
+
+export default shop
