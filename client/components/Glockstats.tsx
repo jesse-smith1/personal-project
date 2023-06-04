c68bd9cd3408a3c984b11d00179b98302c680df9
Jesse <j.t.a.smith26@gmail.com>
2023-06-04 19:48:58 +1200

    updated shop page to be less busy in code

---
+++ client/components/Glockstats.tsx
@@ -0,0 +1,21 @@
+import data from '../Data/data'
+import { useState, ChangeEvent, FormEvent } from 'react'
+
+function Glockstats() {
+  //find a full object that is referecing 1 glock
+  const [glock, setGlockIndex] = useState(1)
+
+  const found = data.find((glocks) => glocks.id === glock)
+  return (
+    <ul>
+      <img src="./client/public/images/Glock1.png" alt="This is a Glock"></img>
+      <li>Name: {found.name}</li>
+      <li>Rarity: {found.rarity}</li>
+      <li>Damage: {found.damage}</li>
+      <img src="./client/public/images/Glock2.png" alt="This is a Glock"></img>
+      <li>Name:{found.name}</li>
+    </ul>
+  )
+}
+
+export default Glockstats
--- client/components/Shop.tsx
+++ client/components/Shop.tsx
@@ -1,34 +1,16 @@
 //importing data from file path Data/data to use on shop page.
 import { useState, ChangeEvent, FormEvent } from 'react'
 import data from '../Data/data'
+import Glockstats from './Glockstats'
 
 function Shop() {
-  //find a full object that is referecing 1 glock
-  const [glock, setGlockIndex] = useState(1)
-
-  const found = data.find((glocks) => glocks.id === glock)
-  console.log(found)
-
   return (
     <>
       <div>
-        <p>Shop1</p>
+        <h1>GlockenSpiel</h1>
       </div>
       <div>
-        <ul>
-          <img
-            src="./client/public/images/Glock1.png"
-            alt="This is a Glock"
-          ></img>
-          <li>Name: {found.name}</li>
-          <li>Rarity: {found.rarity}</li>
-          <li>Damage: {found.damage}</li>
-          <img
-            src="./client/public/images/Glock2.png"
-            alt="This is a Glock"
-          ></img>
-          <li>Name:{found.name}</li>
-        </ul>
+        <Glockstats />
       </div>
     </>
   )
